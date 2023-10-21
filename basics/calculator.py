23d9d8bdf6c2ff132a198500e8b07565c7fdbbe5
saozd <saozdemir1991@gmail.com>
2023-10-22 02:55:42 +0300

    basic

---
+++ basics/calculator.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
+
+
+print("Toplam: " + str(add(10, 15)))
+print("Fark: " + str(sub(15, 10)))
+print("Çarpım: " + str(mul(7, 8)))
+print("Bölüm: " + str(div(100, 25)))
